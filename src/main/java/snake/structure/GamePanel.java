53bb6a8bd832458b98f736af228f890575487f7d
Ewelina <ewelass@gmail.com>
2025-04-23 16:12:40 +0200

    Added GamePanel class

--- src/main/java/snake/structure/GamePanel.java
+++ src/main/java/snake/structure/GamePanel.java
@@ -1,4 +1,34 @@
 package snake.structure;
 
-public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static javax.swing.text.StyleConstants.setBackground;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    private static final int SCREEN_WIDTH = 800;
+    private static final int SCREEN_HEIGHT = 700;
+    private static final int DELAY = 100;
+
+
+    public GamePanel() {
+        setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        setBackground(Color.BLACK);
+        setFocusable(true);
+
+        Timer timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
 }
