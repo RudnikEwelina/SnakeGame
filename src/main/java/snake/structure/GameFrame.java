dc44c658bd4d4c6626d344b07747c31cc29b0233
Ewelina <ewelass@gmail.com>
2025-04-23 15:17:39 +0200

    Ignore workspace.xml from IntelliJ

--- src/main/java/snake/structure/GameFrame.java
+++ src/main/java/snake/structure/GameFrame.java
@@ -1,4 +1,21 @@
 package snake.structure;
 
-public class GameFrame {
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    public GameFrame() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        GamePanel gamePanel = new GamePanel();
+        add(gamePanel);
+        pack();
+        setLocationRelativeTo(null);
+
+    }
+
+    public void start() {
+        setVisible(true);
+    }
 }
